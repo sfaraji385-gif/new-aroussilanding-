2d39c4792986ca9701b1f42a64e1e308c8072e1d
nouronrahmouni <nourbusineess@gmail.com>
2026-06-27 20:30:18 +0100

    add Microsoft Clarity tracking script

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -53,6 +53,17 @@ export default function Analytics() {
           }(window, document, 'ttq');
         `}</Script>
       )}
+
+      {/* Microsoft Clarity Script */}
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "xdqh1jml09");
+        `}
+      </Script>
     </>
   );
 }
